6e968e7ac4b27ff73fb9ad85cc1d95fd117148bc
nv267-svg <nv267@cornell.edu>
2026-06-11 13:19:01 -0400

    Change default port from 8081 to 8080

--- main.py
+++ main.py
@@ -38,5 +38,5 @@ def handle_kagenti_request():
         return jsonify({"error": f"An execution error occurred: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8081))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
